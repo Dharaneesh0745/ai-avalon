d42edd98cf958acacdca9f1f13c17c8c036feb8e
Dharaneesh0745 <dharaneesh0745@gmail.com>
2024-04-30 21:11:19 +0530

    Updated footer text size.

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer class="bg-white shadow dark:bg-gray-900">
-      <div class="w-full max-w-screen-xl lg:text-2xl mx-auto p-4 md:py-8">
+      <div class="w-full max-w-screen-xl lg:text-xl mx-auto p-4 md:py-8">
         {/* <div class="sm:flex sm:items-center sm:justify-between">
             <a
               href="https://flowbite.com/"
