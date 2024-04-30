153bd371e3e55d195fe02b5d054e816eb4add6b6
Dharaneesh0745 <dharaneesh0745@gmail.com>
2024-04-30 19:55:25 +0530

    Initial landing pages.

---
+++ src/app/(sub pages)/contact/page.js
@@ -0,0 +1,42 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+import bg from "../../../../public/background/contact-background.png";
+import Form from "@/components/contact/Form";
+
+export const metadata = {
+  title: "Contact",
+};
+
+export default function Contact() {
+  return (
+    <>
+      <Image
+        src={bg}
+        alt="Next.js Portfolio website's contact page background image"
+        priority
+        sizes="100vw"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
+      />
+
+      <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
+        <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
+          <h1 className="text-accent font-semibold text-center text-4xl capitalize">
+            summon your thoughts!
+          </h1>
+          <p className="text-center font-light text-sm xs:text-base">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        <Form />
+      </article>
+    </>
+  );
+}
---
+++ src/components/about/index.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+import Image from "next/image";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-15 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+            AI AVALON
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro odit
+            iusto et suscipit quibusdam quaerat delectus quas, ducimus,
+            consectetur omnis labore, mollitia placeat perspiciatis alias
+            sapiente quisquam explicabo voluptates soluta.
+          </p>
+        </ItemLayout>
+
+        {/* <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            5 <sub className="font-semibold text-base">Teams</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            10+ <sub className="font-semibold text-base">Achievements</sub>
+          </p>
+        </ItemLayout> */}
+
+        {/* <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          <Image
+            width="1000"
+            height="1000"
+            className="w-full h-auto"
+            src="/background/home-background.png"
+            alt="avalon"
+            loading="lazy"
+          />
+        </ItemLayout> */}
+
+        {/* <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          <img className="w-full h-auto" src="" alt="avalon" loading="lazy" />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full"}>
+          <img className="w-full h-auto" src="" alt="avalon" loading="lazy" />
+        </ItemLayout> */}
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-3 text-accent"}
+        >
+          {/* <img
+            className="w-full h-auto"
+            src=""
+            alt="avalon"
+            loading="lazy"
+          /> */}
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-3 text-accent"}
+        >
+          <Link href="" target="_blank" className="w-full">
+            <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+              {" "}
+              <sub className="font-semibold text-base"></sub>
+            </p>
+            {/* <img
+              className="w-full h-auto"
+              src=""
+              alt="f"
+              loading="lazy"
+            /> */}
+          </Link>
+        </ItemLayout>
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-3 text-accent"}
+        >
+          <Link href="" target="_blank" className="w-full">
+            <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+              {" "}
+              <sub className="font-semibold text-base"></sub>
+            </p>
+            {/* <img
+              className="w-full h-auto"
+              src=""
+              alt="f"
+              loading="lazy"
+            /> */}
+          </Link>
+        </ItemLayout>
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-3 text-accent"}
+        >
+          <Link href="" target="_blank" className="w-full">
+            <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+              {" "}
+              <sub className="font-semibold text-base"></sub>
+            </p>
+            {/* <img
+              className="w-full h-auto"
+              src=""
+              alt="f"
+              loading="lazy"
+            /> */}
+          </Link>
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
